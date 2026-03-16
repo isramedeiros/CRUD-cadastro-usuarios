98a49e06b478dc2013c29c7d10c368961b8431ac
Israel Medeiros <israelmfreitas12@gmail.com>
2026-03-16 18:00:30 -0300

    Inclusao de metodo deletar usuario por e-mail

---
+++ src/main/java/br/com/fiap/cadastro_usuario/business/UsuarioService.java
@@ -0,0 +1,29 @@
+package br.com.fiap.cadastro_usuario.business;
+
+import br.com.fiap.cadastro_usuario.infrastructure.entities.Usuario;
+import br.com.fiap.cadastro_usuario.infrastructure.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository repository;
+
+    public UsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public void salvarUsuario(Usuario usuario) {
+        repository.saveAndFlush(usuario);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email) {
+        return repository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("Email não encontrado")
+        );
+    }
+
+    public void deletarUsuarioPorEmail(String email) {
+        repository.deleteByEmail(email);
+    }
+}
