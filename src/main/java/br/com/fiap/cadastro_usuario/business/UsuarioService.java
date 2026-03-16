a3685d15ab300cd08123fc8d82759fd3d50c2dfb
Israel Medeiros <israelmfreitas12@gmail.com>
2026-03-16 20:51:24 -0300

    Inclusao deletar/atualizar usuario por id

--- src/main/java/br/com/fiap/cadastro_usuario/business/UsuarioService.java
+++ src/main/java/br/com/fiap/cadastro_usuario/business/UsuarioService.java
@@ -26,4 +26,17 @@ public class UsuarioService {
     public void deletarUsuarioPorEmail(String email) {
         repository.deleteByEmail(email);
     }
+
+    public void atualizarUsuarioPorEmail(Integer id, Usuario usuario) {
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(() ->
+                new RuntimeException("Usuario não encontrado"));
+
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+
+        repository.saveAndFlush(usuario);
+    }
 }
--- src/main/java/br/com/fiap/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -12,4 +12,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     @Transactional
     void deleteByEmail(String email);
+
+    Integer email(String email);
 }
